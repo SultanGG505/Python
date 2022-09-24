04e18eee9d13a83efd7c53d144d0f1c98e084090
Sultan <73115468+SultanGG505@users.noreply.github.com>
2022-09-24 08:30:47 +0300

    LR_14_TASK_1 READY

---
+++ LR_14_TASK_1.py
@@ -0,0 +1,50 @@
+# Задание 1. Напишите программу, переводящую градусы по Фаренгейту
+# в градусы по Цельсию. Интерфейс работы с программой представлен ниже.
+
+# master = Tk()
+# master.title("Python")
+#
+# def close(event):
+#     sys.exit()
+#
+# master.bind('<Escape>',close)
+# master.mainloop()
+
+# from Tkinter import *
+# #use tkinter instead of Tkinter (small, not capital T) if it doesn't work
+# #as it was changed to tkinter in newer Python versions
+#
+# root = Tk()
+# Button(root, text="Quit", command=root.destroy).pack() #button to close the window
+# root.mainloop()
+
+import tkinter
+
+
+# Вызывается в момент нажатия на кнопку:
+def click():
+    # Получаем строковое содержимое поля ввода с помощью метода get()
+    # C помощью config() можем изменить отображаемый текст
+    faren = entry.get()
+    # (Фаренгейт — 32): 1, 8 = Цельсий
+    res = (int(faren) - 32) / 1.8
+    label.config(text=res)
+
+
+
+
+window = tkinter.Tk()
+window.title("Перевод Фаренгейт в градусы")
+window.geometry('350x450+700+200')
+frame = tkinter.Frame(window)
+frame.pack(side='top')
+entry = tkinter.Entry(frame)
+entry.pack(side='top')
+label = tkinter.Label(frame)
+label.pack(side='top')
+# Привязываем обработчик нажатия на кнопку к функции click()
+button = tkinter.Button(frame, text='Посчитать градусы в цельсиях!', command=click)
+button.pack(side='top')
+exet = tkinter.Button(window, text='Выйти из программы', command=window.destroy).pack(side='top')
+window.mainloop()
+
--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-num = 1234
-digit = int(str(num)[-2] + str(num)[-1])
-print(digit)
+import tkinter
+window = tkinter.Tk()
+frame = tkinter.Frame(window)
+frame.pack()
+var = tkinter.StringVar()
+# Обновление содержимого переменной происходит в режиме реального времени
+label = tkinter.Label(frame, textvariable=var)
+label.pack()
+# Пробуем набрать текст в появившемся поле для ввода
+6
+entry = tkinter.Entry(frame, textvariable=var)
+entry.pack()
+window.mainloop()
