b63ad799d859ab0f0cd3b8ab024df84e1a8a70da
Sultan <73115468+SultanGG505@users.noreply.github.com>
2022-09-25 13:53:27 +0300

    LR_13_TASK_1 ready

---
+++ LR_13_TASK_1.py
@@ -0,0 +1,14 @@
+class Cat():
+    name = ""
+    color = ""
+    weight = 0
+
+    def meow(self):
+        print(self.name, "мяукнул")
+
+
+myCat = Cat()
+myCat.name = "BARSIK"
+myCat.color = "black"
+myCat.weight = 4
+myCat.meow()
